a210e8b2b460b625d1e6bcb5167d2f9ab255b7ea
Andy Wang <wang.16018@osu.edu>
2026-08-19 14:33:53 -0400

    Add package exports

---
+++ src/workstyle_distance/__init__.py
@@ -0,0 +1,6 @@
+from .embedding import TextEmbedder, workstyle_technology_distance
+from .simulation import simulate_adoption_panel
+from .estimation import age_distance_benchmark, fit_growth_forest
+
+__all__=["TextEmbedder","workstyle_technology_distance","simulate_adoption_panel",
+         "age_distance_benchmark","fit_growth_forest"]
