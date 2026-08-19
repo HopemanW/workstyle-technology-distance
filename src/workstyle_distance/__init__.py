9ab25252d2cf9d227f8780a0306891181dde7324
Andy Wang <wang.16018@osu.edu>
2026-08-19 14:43:48 -0400

    Expose empirical public-data API

--- src/workstyle_distance/__init__.py
+++ src/workstyle_distance/__init__.py
@@ -1,6 +1,52 @@
 from .embedding import TextEmbedder, workstyle_technology_distance
 from .simulation import simulate_adoption_panel
 from .estimation import age_distance_benchmark, fit_growth_forest
+from .official_data import (
+    BLS_INDUSTRY_OCCUPATION_MATRIX_URL,
+    ONET_RELEASE,
+    ONET_TASK_STATEMENTS_URL,
+    ONET_WORK_STYLES_URL,
+    build_occupation_task_corpus,
+    build_occupation_workstyle_matrix,
+    load_onet_task_statements,
+    load_onet_work_styles,
+    normalize_employment_weights,
+)
+from .occupations import (
+    TechnologyMeasurement,
+    measure_technology_distance,
+    predict_technology_occupation_weights,
+    sinkhorn_organizational_distance,
+    workstyle_vector_distance,
+)
+from .patentsview import PatentsViewClient, build_publication_query, publication_documents
+from .census_bds import CensusBDSClient
+from .empirical import build_industry_technology_measurement, measurement_summary
 
-__all__=["TextEmbedder","workstyle_technology_distance","simulate_adoption_panel",
-         "age_distance_benchmark","fit_growth_forest"]
+__all__ = [
+    "TextEmbedder",
+    "workstyle_technology_distance",
+    "simulate_adoption_panel",
+    "age_distance_benchmark",
+    "fit_growth_forest",
+    "ONET_RELEASE",
+    "ONET_WORK_STYLES_URL",
+    "ONET_TASK_STATEMENTS_URL",
+    "BLS_INDUSTRY_OCCUPATION_MATRIX_URL",
+    "load_onet_work_styles",
+    "load_onet_task_statements",
+    "build_occupation_workstyle_matrix",
+    "build_occupation_task_corpus",
+    "normalize_employment_weights",
+    "TechnologyMeasurement",
+    "predict_technology_occupation_weights",
+    "workstyle_vector_distance",
+    "sinkhorn_organizational_distance",
+    "measure_technology_distance",
+    "PatentsViewClient",
+    "build_publication_query",
+    "publication_documents",
+    "CensusBDSClient",
+    "build_industry_technology_measurement",
+    "measurement_summary",
+]
